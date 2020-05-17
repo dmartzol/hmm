fa009b63c85f31af9efb9a7edf1a3f1b6e4da34a
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-05-17 12:17:19 -0500

    improving db design:
    * adding constraints
    * removing redundant field 'reviewed'
    * replacing TIMESTAMP by TIMESTAMPTZ

--- config/postgres/create_tables.sql
+++ config/postgres/create_tables.sql
@@ -21,16 +21,16 @@ CREATE TABLE roles (
     id BIGSERIAL PRIMARY KEY,
     "type" NUMERIC NOT NULL,
     "name" TEXT,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE role_permissions (
     id BIGSERIAL PRIMARY KEY,
     role_id BIGINT REFERENCES roles (id) NOT NULL,
     permission BIGINT NOT NULL,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE accounts (
@@ -38,20 +38,45 @@ CREATE TABLE accounts (
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
     dob date NOT NULL,
-    gender VARCHAR(30) DEFAULT NULL,
+    gender VARCHAR DEFAULT NULL,
     active BOOLEAN NOT NULL DEFAULT FALSE,
     reviewed BOOLEAN NOT NULL DEFAULT FALSE,
     email CITEXT NOT NULL UNIQUE,
-    confirmed_email BOOLEAN DEFAULT FALSE,
-    phone_number VARCHAR(20) UNIQUE DEFAULT NULL,
+    confirmed_email BOOLEAN DEFAULT FALSE CHECK  (confirmed_email OR NOT active),
+    phone_number VARCHAR UNIQUE DEFAULT NULL,
     confirmed_phone BOOLEAN DEFAULT FALSE,
     passhash TEXT NOT NULL,
     failed_logins_count INT DEFAULT 0,
     door_code VARCHAR DEFAULT NULL,
     external_payment_customer_id INT DEFAULT NULL,
-    role_id BIGINT REFERENCES roles (id) DEFAULT NULL,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    review_time TIMESTAMPTZ DEFAULT NULL,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE accounts ADD CONSTRAINT external_payment_restrictions CHECK
+(
+    (confirmed_email AND reviewed)
+    OR
+    (NOT confirmed_email AND external_payment_customer_id IS NULL)
+    OR
+    (NOT reviewed AND external_payment_customer_id IS NULL)
+);
+ALTER TABLE accounts ADD CONSTRAINT active_restrictions CHECK
+(
+    (confirmed_email AND reviewed)
+    OR
+    (NOT confirmed_email AND NOT active)
+    OR
+    (NOT reviewed AND NOT active)
+);
+
+CREATE TABLE account_roles (
+    id BIGSERIAL PRIMARY KEY,
+    role_id BIGINT REFERENCES roles (id) NOT NULL,
+    account_id BIGINT REFERENCES accounts (id) NOT NULL,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE confirmations (
@@ -60,11 +85,11 @@ CREATE TABLE confirmations (
     confirmation_target VARCHAR DEFAULT NULL,
     account_id BIGINT REFERENCES accounts (id) NOT NULL,
     key VARCHAR NOT NULL UNIQUE,
-    confirm_time TIMESTAMP DEFAULT NULL,
+    confirm_time TIMESTAMPTZ DEFAULT NULL,
     failed_confirmations_count INT DEFAULT 0,
-    expire_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP + interval '5 minutes',
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    expire_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP + interval '5 minutes',
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE account_events (
@@ -72,8 +97,8 @@ CREATE TABLE account_events (
     account_id BIGINT REFERENCES accounts (id) NOT NULL,
     "type" NUMERIC NOT NULL,
     note VARCHAR,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE addresses (
@@ -85,17 +110,17 @@ CREATE TABLE addresses (
     street VARCHAR(80) NOT NULL,
     zip_code VARCHAR NOT NULL,
     "type" INT NOT NULL, -- type address and type billing address
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE sessions (
     id BIGSERIAL PRIMARY KEY,
     account_id BIGINT REFERENCES accounts (id) NOT NULL,
-    last_activity_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_activity_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     session_id UUID DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE equipment (
@@ -103,8 +128,8 @@ CREATE TABLE equipment (
     "type" INT NOT NULL,
     "description" text,
     active BOOLEAN NOT NULL DEFAULT TRUE,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE authorizations (
@@ -114,8 +139,8 @@ CREATE TABLE authorizations (
     "type" INT NOT NULL,
     "description" text,
     renewable BOOLEAN NOT NULL,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE account_authorizations (
@@ -123,11 +148,11 @@ CREATE TABLE account_authorizations (
     account_id BIGINT REFERENCES accounts (id) NOT NULL,
     authorization_id BIGINT REFERENCES authorizations (id) NOT NULL,
     active BOOLEAN NOT NULL,
-    efective_time TIMESTAMP,
-    renewal_time TIMESTAMP,
-    expire_time TIMESTAMP,
-    create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    efective_time TIMESTAMPTZ,
+    renewal_time TIMESTAMPTZ,
+    expire_time TIMESTAMPTZ,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMIT;
