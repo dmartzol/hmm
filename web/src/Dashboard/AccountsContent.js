7f786fa56accb97ed4cad671513082eaf7124f0c
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-07-23 23:57:43 -0500

    removes unused components

--- web/src/Dashboard/AccountsContent.js
+++ web/src/Dashboard/AccountsContent.js
@@ -13,13 +13,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
 
-import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import RefreshIcon from '@material-ui/icons/Refresh';
 
 
 const columns = [
