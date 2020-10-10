f346aed10f59e6b16c511ce97dfed072e573b71a
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-10 15:56:39 -0500

    changes indentation to 2 spaces

--- web/src/Dashboard/Footer.js
+++ web/src/Dashboard/Footer.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function Footer() {
 
-    return (
-        <footer class="container-fluid mt-5">
-            <div className="row">
-                <div className="container-fluid text-center text-secondary">
-                    Hmm! © {new Date().getFullYear()}{'.'}
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer class="container-fluid mt-5">
+      <div className="row">
+        <div className="container-fluid text-center text-secondary">
+          Hmm! © {new Date().getFullYear()}{'.'}
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- web/src/Landing/Landing.js
+++ web/src/Landing/Landing.js
@@ -4,68 +4,68 @@ import { Link } from 'react-router-dom';
 import axios from "axios";
 
 function Landing() {
-    const initialUserState = {
-        loggedIn: false,
-        session: {
-            AccountID: 0,
-            LastActivityTime: "",
-        },
-    };
-    const [session, setSession] = useState(initialUserState)
+  const initialUserState = {
+    loggedIn: false,
+    session: {
+      AccountID: 0,
+      LastActivityTime: "",
+    },
+  };
+  const [session, setSession] = useState(initialUserState)
 
-    useEffect(() => {
-        const getSession = async () => {
-            axios.get(
-                "http://localhost:3001/v1/sessions",
-                { withCredentials: true }
-            ).then(response => {
-                if (response.error) {
-                    return
-                }
-                if (response.status === 200) {
-                    setSession({
-                        loggedIn: true,
-                        session: response.data,
-                    });
-                }
-            });
+  useEffect(() => {
+    const getSession = async () => {
+      axios.get(
+        "http://localhost:3001/v1/sessions",
+        { withCredentials: true }
+      ).then(response => {
+        if (response.error) {
+          return
         }
-        getSession()
-    }, [])
+        if (response.status === 200) {
+          setSession({
+            loggedIn: true,
+            session: response.data,
+          });
+        }
+      });
+    }
+    getSession()
+  }, [])
 
 
-    return (
-        <div className="container-fluid p-0">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="/">Hmm!</a>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li>
-                            {
-                                session.loggedIn ?
-                                    <div>
-                                        <Button component={Link} to={'/me'} color="inherit">My account</Button>
-                                        <Button component={Link} to={'/logout'} color="inherit">Logout</Button>
-                                    </div> :
-                                    <div>
-                                        <Button component={Link} to={'/login'} color="inherit">Login</Button>
-                                        <Button component={Link} to={'/signup'} color="inherit">Sign Up</Button>
-                                    </div>
-                            }
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-            <div className="container-fluid bg-light d-flex justify-content-center align-items-center min-vh-100">
-                <h1>Your Content</h1>
-            </div>
+  return (
+    <div className="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">Hmm!</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li>
+              {
+                session.loggedIn ?
+                  <div>
+                    <Button component={Link} to={'/me'} color="inherit">My account</Button>
+                    <Button component={Link} to={'/logout'} color="inherit">Logout</Button>
+                  </div> :
+                  <div>
+                    <Button component={Link} to={'/login'} color="inherit">Login</Button>
+                    <Button component={Link} to={'/signup'} color="inherit">Sign Up</Button>
+                  </div>
+              }
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
         </div>
+      </nav>
+      <div className="container-fluid bg-light d-flex justify-content-center align-items-center min-vh-100">
+        <h1>Your Content</h1>
+      </div>
+    </div>
 
-    );
+  );
 }
 
 export default Landing;
