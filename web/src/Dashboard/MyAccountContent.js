3c5911e3b40df8fc66bd51469d6eb18317b96bc7
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-10 02:28:17 -0500

    redirecting from /me to /accounts/id

--- web/src/Dashboard/MyAccountContent.js
+++ web/src/Dashboard/MyAccountContent.js
@@ -3,53 +3,36 @@ import axios from 'axios'
 
 export default function UserPage(props) {
 
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState({})
+    const initialStates = {
+    loggedIn: false,
+    session: {
+      AccountID: 0,
+      LastActivityTime: "",
+    },
+  };
+  const [session, setSession] = useState(initialStates);
   useEffect(() => {
-    const getMyAccount = async () => {
+    const getSession = async () => {
       axios.get(
-        "http://localhost:3001/v1/accounts/" + props.session.session.AccountID,
+        "http://localhost:3001/v1/sessions",
         { withCredentials: true }
       ).then(response => {
         if (response.status === 200) {
-          setUser(
-            response.data,
-          );
-          setLoading(false);
+          setSession({
+            loggedIn: true,
+            session: response.data,
+          });
+          props.history.push('/accounts/' + response.data.AccountID);
         }
+      }).catch(error => {
+        props.history.push('/');
+        return
       });
     }
+    getSession()
+  },[props])
 
-    if (props.isAuthed) {
-      getMyAccount()
-    }
-  }, [props])
-
-  return loading ? (
+  return (
     <div align="center">Loading...</div>
-  ) : (
-      <div className="container">
-        <h1>{props.match.params.id}</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Last Name</th>
-              <th>Date of birth</th>
-              <th>Email</th>
-              <th>Gender</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{user.FirstName}</td>
-              <td>{user.LastName}</td>
-              <td>{user.DateOfBird}</td>
-              <td><a href={user.Email}>{user.Email}</a></td>
-              <td>{user.Gender}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
     )
 }
