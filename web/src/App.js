4089c7821fa7adccea2a3b56816feb45b731e6ba
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-09 20:54:19 -0500

    simplifies admin panel

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,7 @@ import Landing from "./Landing/Landing";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './Landing/Login';
 import Logout from './Landing/Logout';
-import Paperbase from './Dashboard/Paperbase';
+import AdminPanel from './Dashboard/AdminPanel';
 
 export default class App extends Component {
 
@@ -15,11 +15,11 @@ export default class App extends Component {
         <Route exact path={"/login"} render={props => (<Login />)} />
         <Route exact path={"/logout"} render={props => (<Logout />)} />
         <Route exact path={"/signup"} render={props => (<Logout />)} />
-        <Route exact path={"/me"} render={props => (<Paperbase />)} />
-        <Route exact path={"/accounts"} render={props => (<Paperbase />)} />
-        <Route exact path={"/authorizations"} render={props => (<Paperbase />)} />
-        <Route exact path={"/equipment"} render={props => (<Paperbase />)} />
-        <Route exact path={"/roles"} render={props => (<Paperbase />)} />
+        <Route exact path={"/me"} render={props => (<AdminPanel />)} />
+        <Route exact path={"/accounts"} render={props => (<AdminPanel />)} />
+        <Route exact path={"/authorizations"} render={props => (<AdminPanel />)} />
+        <Route exact path={"/equipment"} render={props => (<AdminPanel />)} />
+        <Route exact path={"/roles"} render={props => (<AdminPanel />)} />
       </Switch>
   </BrowserRouter>
     );
---
+++ web/src/Dashboard/AdminPanel.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import Navigator from './Navigator';
+import AuthorizationsContent from './AuthorizationsContent';
+import AccountsContent from './AccountsContent';
+import MyAccountContent from './MyAccountContent';
+import Header from './Header';
+import axios from "axios";
+import { Route, Switch, withRouter } from 'react-router-dom';
+
+function Copyright() {
+  return (
+    <div className="container-fluid text-center text-secondary">
+      Copyright © {new Date().getFullYear()}{'.'}
+    </div>
+  );
+}
+
+
+function AdminPanel(props) {
+  const initialStates = {
+    loggedIn: false,
+    session: {
+      AccountID: 0,
+      LastActivityTime: "",
+    },
+  };
+  const [session, setSession] = React.useState(initialStates);
+
+  React.useEffect(() => {
+    const getSession = async () => {
+      axios.get(
+        "http://localhost:3001/v1/sessions",
+        { withCredentials: true }
+      ).then(response => {
+        if (response.status === 200) {
+          setSession({
+            loggedIn: true,
+            session: response.data,
+          });
+        }
+      }).catch(error => {
+        props.history.push('/');
+        return
+      });
+    }
+    getSession()
+  }, [props])
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-2 bg-dark min-vh-100">
+          <Navigator />
+        </div>
+        <div className="col-10 bg-light p-0">
+          <Header />
+          <Switch>
+            <Route exact path="/accounts" render={(props) => <AccountsContent {...props} session={session} isAuthed={session.loggedIn} />} />
+            <Route exact path="/me" render={(props) => <MyAccountContent {...props} session={session} isAuthed={session.loggedIn} />} />
+            <Route exact path="/authorizations" component={AuthorizationsContent} />
+          </Switch>
+          <footer className="mt-5">
+            <Copyright />
+          </footer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(AdminPanel);
