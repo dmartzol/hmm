743794c0e3893e1f9b27abbb91345922aee86702
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-09 21:23:31 -0500

    simplifying header

--- web/src/Dashboard/Header.js
+++ web/src/Dashboard/Header.js
@@ -1,59 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
-import { withStyles } from '@material-ui/core/styles';
 
-const lightColor = 'rgba(255, 255, 255, 0.7)';
-
-const styles = (theme) => ({
-  secondaryBar: {
-    zIndex: 0,
-  },
-  menuButton: {
-    marginLeft: -theme.spacing(1),
-  },
-  iconButtonAvatar: {
-    padding: 4,
-  },
-  link: {
-    textDecoration: 'none',
-    color: lightColor,
-    '&:hover': {
-      color: theme.palette.common.white,
-    },
-  },
-  button: {
-    borderColor: lightColor,
-  },
-});
-
-function Header(props) {
-  const { classes } = props;
+function Header() {
 
   return (
-    <React.Fragment>
-      <AppBar color="primary" position="sticky" elevation={0}>
-      </AppBar>
-      <AppBar
-        component="div"
-        className={classes.secondaryBar}
-        color="primary"
-        position="static"
-        elevation={0}
-      >
-        <Tabs value={0} textColor="inherit">
-          <Tab textColor="inherit" label="List" />
-        </Tabs>
-      </AppBar>
-    </React.Fragment>
+    <ul class="nav justify-content-end bg-secondary mb-5">
+      <li class="nav-item"><a class="nav-link btn-secondary mr-5 my-1" href="/me">Help</a></li>
+    </ul>
   );
 }
 
-Header.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onDrawerToggle: PropTypes.func.isRequired,
-};
-
-export default withStyles(styles)(Header);
+export default Header;
