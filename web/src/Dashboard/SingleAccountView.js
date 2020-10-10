18c0419f798b719db62375727db9b848f74fa4cb
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-10 15:25:55 -0500

    improves single account view

--- web/src/Dashboard/SingleAccountView.js
+++ web/src/Dashboard/SingleAccountView.js
@@ -3,46 +3,54 @@ import axios from 'axios';
 import { SingleAccountFields } from '../constants'
 
 export default function SingleAccount(props) {
-    const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState({})
-    useEffect(() => {
-        const getMyAccount = async () => {
-            axios.get(
-                "http://localhost:3001/v1/accounts/" + props.match.params.id,
-                { withCredentials: true }
-            ).then(response => {
-                if (response.status === 200) {
-                    setUser(
-                        response.data,
-                    );
-                    setLoading(false);
-                }
-            });
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    const getMyAccount = async () => {
+      axios.get(
+        "http://localhost:3001/v1/accounts/" + props.match.params.id,
+        { withCredentials: true }
+      ).then(response => {
+        if (response.status === 200) {
+          setUser(
+            response.data,
+          );
+          setLoading(false);
         }
+      });
+    }
 
-        if (props.isAuthed) {
-            getMyAccount()
-        }
-    }, [props])
+    if (props.isAuthed) {
+      getMyAccount()
+    }
+  }, [props])
 
-    return loading ? (
-        <div align="center">Loading...</div>
-    ) : (
-            <div className="container">
-                <h1>{user.FirstName} {user.LastName} #{props.match.params.id}</h1>
-                <table>
-                    <tbody>
-                        {
-                            SingleAccountFields.map((accountField) => {
-                                const value = user[accountField.id];
-                                return (
-                                    <tr><td>{accountField.id}: {accountField.formatedCell ? accountField.format(value) : value}</td></tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-        )
+  return loading ? (
+    <div align="center">Loading...</div>
+  ) : (
+      <div className="container border">
+        <div className="row d-block">
+          <div className="title">
+            <h1>{user.FirstName} {user.LastName}</h1>
+          </div>
+          <table>
+            <tbody>
+              {
+                SingleAccountFields.map((accountField) => {
+                  const value = user[accountField.id];
+                  return (
+                    <tr>
+                      <td>{accountField.id}:</td>
+                      <td>{accountField.formatedCell ? accountField.format(value) : value}</td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+
+      </div>
+    )
 }
 
