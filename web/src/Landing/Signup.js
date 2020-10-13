e2c3320052384fe09a82dcecbfa6c3646f156bfc
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-12 23:33:29 -0500

    adds city to signup form

--- web/src/Landing/Signup.js
+++ web/src/Landing/Signup.js
@@ -20,17 +20,17 @@ class Signup extends Component {
                                 <div class="row">
                                     <div class="col-md-4 mb-3">
                                         <label for="firstName">First name</label>
-                                        <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
+                                        <input type="text" class="form-control" id="firstName" required />
                                         <div class="invalid-feedback">Valid first name is required.</div>
                                     </div>
                                     <div class="col-md-4 mb-3">
-                                        <label for="firstName">Middle name</label>
-                                        <input type="text" class="form-control" id="firstName" placeholder="" value="" required />
+                                        <label for="middlename">Middle name</label>
+                                        <input type="text" class="form-control" id="middlename" />
                                         <div class="invalid-feedback">Valid first name is required.</div>
                                     </div>
                                     <div class="col-md-4 mb-3">
                                         <label for="lastName">Last name</label>
-                                        <input type="text" class="form-control" id="lastName" placeholder="" value="" required />
+                                        <input type="text" class="form-control" id="lastName" required />
                                         <div class="invalid-feedback">Valid last name is required.</div>
                                     </div>
                                 </div>
@@ -64,7 +64,7 @@ class Signup extends Component {
                                 </div>
 
                                 <div class="row">
-                                    <div class="col-md-5 mb-3">
+                                    <div class="col-md-3 mb-3">
                                         <label for="country">Country</label>
                                         <select class="custom-select d-block w-100" id="country" required>
                                             <option value="">Choose...</option>
@@ -72,17 +72,22 @@ class Signup extends Component {
                                         </select>
                                         <div class="invalid-feedback">Please select a valid country.</div>
                                     </div>
-                                    <div class="col-md-4 mb-3">
+                                    <div class="col-md-3 mb-3">
                                         <label for="state">State</label>
                                         <select class="custom-select d-block w-100" id="state" required>
                                             <option value="">Choose...</option>
                                             <option>California</option>
-                                            <option>California</option>
-                                            <option>California</option>
+                                            <option>Illinois</option>
+                                            <option>New york</option>
                                         </select>
                                         <div class="invalid-feedback">Please provide a valid state.</div>
                                     </div>
-                                    <div class="col-md-3 mb-3">
+                                    <div class="col-md-4 mb-3">
+                                        <label for="city">City</label>
+                                        <input type="text" class="form-control" id="city" placeholder="" required />
+                                        <div class="invalid-feedback">City required.</div>
+                                    </div>
+                                    <div class="col-md-2 mb-3">
                                         <label for="zip">Zip</label>
                                         <input type="text" class="form-control" id="zip" placeholder="" required />
                                         <div class="invalid-feedback">Zip code required.</div>
