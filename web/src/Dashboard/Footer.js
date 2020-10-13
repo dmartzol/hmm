369aa9e70b591d3a3eef7090d8ef5dc8ee7bb2a0
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2020-10-12 22:38:34 -0500

    sad and futile attempt to make footer stick to bottom

--- web/src/Dashboard/Footer.js
+++ web/src/Dashboard/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer() {
 
   return (
-    <footer class="container-fluid mt-5">
-      <div className="row">
+    <footer class="footer mt-5 py-3">
+      <div className="container">
         <div className="container-fluid text-center text-secondary">
           Hmm! © {new Date().getFullYear()}{'.'}
         </div>
