38abd68c6cc2d9d8cc7971c9ade7bfdd6ceecf99
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2022-02-08 19:48:39 -0600

    removes unnecessary migrations

--- migrations/001.up.sql
+++ migrations/001.up.sql
@@ -2,7 +2,6 @@ BEGIN;
 CREATE EXTENSION IF NOT EXISTS citext;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE accounts (
     id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR NOT NULL,
@@ -22,7 +21,6 @@ CREATE TABLE accounts (
     create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
 CREATE TABLE sessions (
     id BIGSERIAL PRIMARY KEY,
     account_id BIGINT REFERENCES accounts (id) NOT NULL,
@@ -32,5 +30,79 @@ CREATE TABLE sessions (
     create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
+CREATE TABLE roles (
+    id BIGSERIAL PRIMARY KEY,
+    "name" TEXT UNIQUE,
+    permission_bit BIGINT NOT NULL CHECK (permission_bit >= 0),
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE account_roles (
+    id BIGSERIAL PRIMARY KEY,
+    role_id BIGINT REFERENCES roles (id) NOT NULL,
+    account_id BIGINT REFERENCES accounts (id) NOT NULL,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (role_id, account_id)
+);
+CREATE TABLE confirmations (
+    id BIGSERIAL PRIMARY KEY,
+    "type" NUMERIC NOT NULL, -- email, phone number or password reset
+    confirmation_target VARCHAR DEFAULT NULL,
+    account_id BIGINT REFERENCES accounts (id) NOT NULL,
+    key VARCHAR NOT NULL UNIQUE,
+    confirm_time TIMESTAMPTZ DEFAULT NULL,
+    failed_confirmations_count INT DEFAULT 0,
+    expire_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP + interval '5 hours',
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE account_events (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT REFERENCES accounts (id) NOT NULL,
+    "type" NUMERIC NOT NULL,
+    note VARCHAR,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE addresses (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT REFERENCES accounts (id) UNIQUE NOT NULL,
+    country VARCHAR(20) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state_code VARCHAR(2) NOT NULL,
+    street VARCHAR(80) NOT NULL,
+    zip_code VARCHAR NOT NULL,
+    "type" INT NOT NULL, -- type address and type billing address
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE equipment (
+    id BIGSERIAL PRIMARY KEY,
+    "type" INT NOT NULL,
+    "description" text,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE authorizations (
+    id BIGSERIAL PRIMARY KEY,
+    equipment_id BIGINT REFERENCES equipment (id) NOT NULL,
+    "type" INT NOT NULL,
+    "description" text,
+    renewable BOOLEAN NOT NULL,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE account_authorizations (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT REFERENCES accounts (id) NOT NULL,
+    authorization_id BIGINT REFERENCES authorizations (id) NOT NULL,
+    active BOOLEAN NOT NULL,
+    efective_time TIMESTAMPTZ,
+    renewal_time TIMESTAMPTZ,
+    expire_time TIMESTAMPTZ,
+    create_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 COMMIT;
