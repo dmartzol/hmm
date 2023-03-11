c91a2cbc6d359b42025a7983456a05b046a1d77d
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-10 18:46:17 -0600

    WIP

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/dmartzol/go-sdk/flags"
+	"github.com/dmartzol/go-sdk/logger"
+	"github.com/dmartzol/hmm/cmd/backend/api"
+	"github.com/dmartzol/hmm/internal/storage/postgres"
 	"github.com/urfave/cli"
 )
+
+const (
+	appName = "backend"
+)
+
+var sdkLogger logger.Logger
+
+func init() {
+	sdkLogger = logger.New()
+}
 
 const (
 	flagPort              = "port"
@@ -20,55 +35,43 @@ const (
 
 func main() {
 	app := &cli.App{
-		Name:  "gateway",
-		Usage: "",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:   flagHost,
-				EnvVar: "HOST",
-				Value:  "0.0.0.0",
-			},
-			&cli.StringFlag{
-				Name:   flagPort,
-				EnvVar: "PORT",
-				Value:  "1100",
-			},
-			&cli.BoolTFlag{
-				Name:   flagStructuredLogging,
-				EnvVar: "STRUCTURED_LOGGING",
-			},
-			&cli.StringFlag{
-				Name:   flagDBName,
-				EnvVar: "PGDATABASE",
-				Value:  "hmm-development",
-			},
-			&cli.StringFlag{
-				Name:   flagDBUser,
-				EnvVar: "PGUSER",
-				Value:  "user-development",
-			},
-			&cli.StringFlag{
-				Name:   flagDBPort,
-				EnvVar: "DBPORT",
-				Value:  "5432",
-			},
-			&cli.StringFlag{
-				Name:   flagDBPass,
-				EnvVar: "PGPASSWORD",
-				Value:  "",
-			},
-			&cli.StringFlag{
-				Name:   flagDBHost,
-				EnvVar: "PGHOST",
-				Value:  "database",
-			},
-		},
+		Name:   appName,
 		Action: newHmmServiceRun,
 	}
+	app.Flags = append(app.Flags, flags.DatabaseFlags...)
+	app.Flags = append(app.Flags, flags.LoggerFlags...)
+	app.Flags = append(app.Flags, flags.ServerFlags...)
 
 	err := app.Run(os.Args)
+	if err != nil {
+		sdkLogger.Errorf("error running app: %v", err)
+	}
+}
+
+func newHmmServiceRun(c *cli.Context) error {
+	port := c.String(flagPort)
+	host := c.String(flagHost)
+	structuredLogging := c.Bool(flagStructuredLogging)
+
+	dbConfig := postgres.Config{
+		Host:     c.String(flagDBHost),
+		Port:     c.Int(flagDBPort),
+		Name:     c.String(flagDBName),
+		User:     c.String(flagDBUser),
+		Password: c.String(flagDBPass),
+	}
+	db, err := postgres.New(dbConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	address := host + ":" + port
+	restAPI := api.NewAPI(structuredLogging, db)
+	server := &http.Server{
+		Addr:    address,
+		Handler: restAPI,
+	}
+
+	restAPI.Logger.Infof("listening and serving on %s", address)
+	return server.ListenAndServe()
 }
