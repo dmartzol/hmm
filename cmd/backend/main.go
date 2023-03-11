38707dd8bab7f08eab7f2665edbdfa54471059ae
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-10 23:21:34 -0600

    moves function and removes unused file

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/dmartzol/hmm/cmd/backend/api"
+	"github.com/dmartzol/hmm/internal/dao/postgres"
 	"github.com/urfave/cli"
 )
 
@@ -71,4 +74,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func newHmmServiceRun(c *cli.Context) error {
+	port := c.String(flagPort)
+	host := c.String(flagHost)
+	structuredLogging := c.Bool(flagStructuredLogging)
+
+	dbConfig := postgres.Config{
+		Host:     c.String(flagDBHost),
+		Port:     c.Int(flagDBPort),
+		Name:     c.String(flagDBName),
+		User:     c.String(flagDBUser),
+		Password: c.String(flagDBPass),
+	}
+	db, err := postgres.New(dbConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := host + ":" + port
+	restAPI := api.NewAPI(structuredLogging, db)
+	server := &http.Server{
+		Addr:    address,
+		Handler: restAPI,
+	}
+
+	restAPI.Logger.Infof("listening and serving on %s", address)
+	return server.ListenAndServe()
 }
