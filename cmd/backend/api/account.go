4e1c2a685ddffcb1fda2c174dcd4998b4c46fb70
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-12 13:48:32 -0500

    moves function

--- cmd/backend/api/account.go
+++ cmd/backend/api/account.go
@@ -1,10 +1,8 @@
 package api
 
 import (
-	"crypto/rand"
 	"database/sql"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -142,20 +140,6 @@ func (r *CreateAccountRequest) normalize() error {
 		}
 	}
 	return nil
-}
-
-func randomCode(max int) (string, error) {
-	var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-	b := make([]byte, max)
-	n, err := io.ReadAtLeast(rand.Reader, b, max)
-	if n != max {
-		log.Printf("ReadAtLeast: %+v", err)
-		return "", err
-	}
-	for i := 0; i < len(b); i++ {
-		b[i] = table[int(b[i])%len(table)]
-	}
-	return string(b), nil
 }
 
 func (re Resources) CreateAccount(w http.ResponseWriter, r *http.Request) {
--- cmd/backend/api/util.go
+++ cmd/backend/api/util.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -20,4 +23,18 @@ func normalizeName(name string) string {
 	name = strings.TrimSpace(name)
 	name = strings.ToLower(name)
 	return name
+}
+
+func randomCode(max int) (string, error) {
+	var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, max)
+	n, err := io.ReadAtLeast(rand.Reader, b, max)
+	if n != max {
+		log.Printf("ReadAtLeast: %+v", err)
+		return "", err
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b), nil
 }
