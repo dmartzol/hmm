9d02179ebc149f0b0019d084035383cf33141269
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-12 12:47:22 -0500

    uses Fprint instead of Fprintf

--- pkg/httpresponse/httpresponse.go
+++ pkg/httpresponse/httpresponse.go
@@ -31,7 +31,7 @@ func RespondText(w http.ResponseWriter, text string, code int) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
-	fmt.Fprintf(w, text)
+	fmt.Fprint(w, text)
 }
 
 func RespondJSON(w http.ResponseWriter, object interface{}) {
