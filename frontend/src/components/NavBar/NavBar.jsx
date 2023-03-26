a56183185b2b6cdda50fa8e1607574051a5f60b4
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-26 01:02:09 -0500

    removes unused code

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,6 @@
 import React from "react";
-import cx from "classnames";
 
 function NavBar() {
-  const setDebugMode = (classStr) => {
-    const debugMode = true;
-    return debugMode ? `${classStr} border-gray-700 border-2` : classStr;
-  };
-
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -23,7 +17,7 @@ function NavBar() {
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">Hmm</span>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900" href="/">
