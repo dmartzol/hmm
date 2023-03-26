41ce571a16379f188844454018e24bd129600a57
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-25 19:31:57 -0500

    new frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
