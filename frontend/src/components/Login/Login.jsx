247355b6fb239d30cada742f8ebe57bc63537d18
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-25 21:26:07 -0500

    improves login page

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,16 +6,15 @@ function EmptyComponent() {
       <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
         <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
           <h1 class="title-font font-medium text-3xl text-gray-900">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-            numquam labore molestiae nobis odio, illum harum natus voluptatem
-            culpa. Ullam omnis enim laudantium eaque exercitationem cupiditate
-            fugiat, quidem non error.
+            Please, log in to conitnue. If you are a new user,{" "}
+            <a href="/signup">click here</a>. If you have any questions or need
+            help, please contact us by clicking <a href="/contact">here</a>.
           </h1>
         </div>
         <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
           <div class="relative mb-4">
             <label for="full-name" class="leading-7 text-sm text-gray-600">
-              email
+              Email
             </label>
             <input
               type="text"
@@ -26,7 +25,7 @@ function EmptyComponent() {
           </div>
           <div class="relative mb-4">
             <label for="password" class="leading-7 text-sm text-gray-600">
-              password
+              Password
             </label>
             <input
               type="email"
@@ -39,7 +38,8 @@ function EmptyComponent() {
             Login
           </button>
           <p class="text-xs text-gray-500 mt-3">
-            Forgot your passowrd? Click nowhere bc it is not implemented.
+            Forgot your password?
+            <a href="/forgot"> Click here.</a>
           </p>
         </div>
       </div>
