6dbf7a64485eb9671d548e52b318bec3f543458f
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-03-26 11:20:19 -0500

    improves login page

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -31,7 +31,7 @@ function Login() {
               id="full-name"
               name="full-name"
               placeholder="Your email address"
-              defaultValue="example@example.com"
+              defaultValue="andrew@example.com"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
