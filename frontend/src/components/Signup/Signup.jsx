5f05bbae073cf6f37c38123472b29004668315f5
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-04-20 18:59:45 -0500

    form validation wip

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,17 +1,41 @@
-function Signup() {
-  async function signupRequest() {
-    // setIsLoading(true);
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
+function Signup() {
+  const formSchema = yup
+    .object()
+    .shape({
+      firstname: yup.string().required(),
+      lastname: yup.string().required(),
+      email: yup.string().required(),
+      password: yup.string().required(),
+      dob: yup.string().required(),
+    })
+    .required();
+
+  const { register, handleSubmit, errors } = useForm();
+
+  async function onSubmit(data) {
+    // Check the schema if form is valid:
+    const isFormValid = await formSchema.isValid(data, {
+      abortEarly: false, // Prevent aborting validation after first error
+    });
+
+    if (isFormValid) {
+      // If form is valid, continue submission.
+      console.log("Form is legit");
+    } else {
+      // If form is not valid, check which fields are incorrect:
+      formSchema.validate(data, { abortEarly: false }).catch((err) => {
+        console.log(err);
+      });
+    }
+
+    alert(JSON.stringify(data));
     try {
       const response = await fetch(`/accounts`, {
         method: "POST",
-        body: JSON.stringify({
-          firstname: "andrew",
-          lastname: "smith",
-          email: "andrew@example.com",
-          password: "development",
-          dob: "1985-01-01",
-        }),
+        body: JSON.stringify(data),
         headers: {
           "Content-Type": "application/json",
         },
@@ -23,11 +47,12 @@ function Signup() {
     } catch (error) {
       console.error(error);
     }
+    console.log(errors);
   }
 
   return (
     <div className="md:container md:mx-auto md:px-80 sm:px-4">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-12">
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -41,13 +66,21 @@ function Signup() {
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                     <input
+                      id="email"
                       type="email"
                       name="email"
-                      id="email"
                       autoComplete="email"
                       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                       placeholder="andrew@example.com"
+                      {...register("email", {
+                        required: true,
+                        pattern:
+                          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      })}
                     />
+                    <span className="mt-2 hidden text-sm text-red-500">
+                      Please enter a valid email address
+                    </span>
                   </div>
                 </div>
               </div>
@@ -69,18 +102,22 @@ function Signup() {
                 </label>
                 <div className="mt-2">
                   <input
+                    id="first-name"
                     type="text"
                     name="first-name"
-                    id="first-name"
                     autoComplete="given-name"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("firstName", {
+                      required: true,
+                      maxLength: 80,
+                    })}
                   />
                 </div>
               </div>
 
               <div className="sm:col-span-3">
                 <label
-                  htmlFor="last-name"
+                  htmlFor="lastname"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Last name
@@ -88,10 +125,14 @@ function Signup() {
                 <div className="mt-2">
                   <input
                     type="text"
-                    name="last-name"
-                    id="last-name"
+                    name="lastname"
+                    id="lastname"
                     autoComplete="family-name"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("lastName", {
+                      required: true,
+                      maxLength: 100,
+                    })}
                   />
                 </div>
               </div>
@@ -126,11 +167,15 @@ function Signup() {
                 </label>
                 <div className="mt-2">
                   <input
+                    id="street-address"
                     type="text"
                     name="street-address"
-                    id="street-address"
                     autoComplete="street-address"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("street-address", {
+                      required: true,
+                      maxLength: 500,
+                    })}
                   />
                 </div>
               </div>
@@ -149,6 +194,10 @@ function Signup() {
                     id="city"
                     autoComplete="address-level2"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("city", {
+                      required: true,
+                      maxLength: 80,
+                    })}
                   />
                 </div>
               </div>
@@ -167,6 +216,10 @@ function Signup() {
                     id="region"
                     autoComplete="address-level1"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("state", {
+                      required: true,
+                      maxLength: 15,
+                    })}
                   />
                 </div>
               </div>
@@ -185,6 +238,10 @@ function Signup() {
                     id="postal-code"
                     autoComplete="postal-code"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    {...register("postal-code", {
+                      required: true,
+                      maxLength: 1,
+                    })}
                   />
                 </div>
               </div>
@@ -196,7 +253,6 @@ function Signup() {
           <button
             type="submit"
             className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={signupRequest}
           >
             Submit
           </button>
