bb0d385f2e6683dbf3ecc44f874da0152bea1421
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2023-04-22 19:48:16 -0500

    adds password to form

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -13,6 +13,10 @@ function Signup() {
         .lowercase()
         .email("must be a valid email")
         .required("email is required"),
+      password: yup
+        .string()
+        .min(10, "password must have at least 10 characters")
+        .required(),
       state: yup.string().lowercase(),
       city: yup.string().lowercase(),
       street_address: yup.string().lowercase(),
@@ -97,6 +101,29 @@ function Signup() {
                   </div>
                   <p className=" text-red-600 text-sm">
                     {errors.email?.message}
+                  </p>
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      id="password"
+                      type="password"
+                      name="password"
+                      autoComplete="password"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      {...register("password")}
+                    />
+                  </div>
+                  <p className=" text-red-600 text-sm">
+                    {errors.password?.message}
                   </p>
                 </div>
               </div>
